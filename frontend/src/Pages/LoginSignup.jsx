a0774edbe97d80f09fbd414b38bb7f293c249bc3
Mahabub Alam Bhuiyan <ma.nahid960@gmail.com>
2025-01-05 00:25:34 +0600

    product page , 1:54 m

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './css/loginSignup.css'
 
 export default function LoginSignup() {
   return (
     <div>
-      <div>Login and Signup</div>
+      <div className='loginsignup'>
+        <div className="loginsignup-container">
+          <h1>Sign Up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" name="name" id="" placeholder='Your Name' />
+            <input type="email" name="email" id="" placeholder='Email Address' />
+            <input type="password" name="password" id="" placeholder='Password' />
+          </div>
+          <button>Continue</button>
+          <p className='loginsignup-login'>Already have an account? <span>Login Here</span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='checkbox' id='' className='checkbox'/>
+            <p>By continuing, I agree to the terms of use & privacy policy</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
